4a9db254e84e110e7f5b617ff0e56f91cee797c5
Rgh2k4 <146884367+Rgh2k4@users.noreply.github.com>
2024-12-03 16:12:27 -0700

    sprint 3 p15

--- app/movie-list.js
+++ app/movie-list.js
@@ -7,7 +7,7 @@ const Item = ({ movie, onSelect }) => {
   return (
     <li className="p-2 border-b border-gray-300">
       <div>
-        <h3>{title}</h3>
+        <h3 className=" font-bold ">{title}</h3>
         <p>Rating: {vote_average}/10</p>
       </div>
       <button onClick={() => onSelect()} className="text-blue-500 underline">
@@ -17,8 +17,10 @@ const Item = ({ movie, onSelect }) => {
   );
 };
 
-const MovieList = ({ movies, filterRating, filterGenre }) => {
+const MovieList = ({ movies }) => {
   const [sortBy, setSortBy] = useState("");
+  const [filterRating, setFilterRating] = useState("");
+  const [filterGenre, setFilterGenre] = useState("");
   const [filteredMovies, setFilteredMovies] = useState(movies);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [selectedMovieDetails, setSelectedMovieDetails] = useState(null);
@@ -97,7 +99,7 @@ const MovieList = ({ movies, filterRating, filterGenre }) => {
     return (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
         <div className="bg-white p-4 rounded-lg text-black">
-          <h2 className="text">{movie.title}</h2>
+          <h2>{movie.title}</h2>
           <p>Synopsis: {details.overview}</p>
           <p>Runtime: {details.runtime} minutes</p>
           <img
@@ -126,51 +128,51 @@ const MovieList = ({ movies, filterRating, filterGenre }) => {
 
   return (
     <div className="flex flex-col gap-4">
+      <div className="flex gap-4 mb-4">
+        <select
+          value={filterRating}
+          onChange={(e) => setFilterRating(e.target.value)}
+          className="flex border border-gray-400 p-2 text-black"
+        >
+          <option value="">Age Rating</option>
+          <option value="G">G</option>
+          <option value="PG">PG</option>
+          <option value="14A">14A</option>
+          <option value="18A">18A</option>
+          <option value="A">A</option>
+        </select>
+
+        <select
+          value={filterGenre}
+          onChange={(e) => setFilterGenre(e.target.value)}
+          className="flex border border-gray-400 p-2 text-black"
+        >
+          <option value="">Genre</option>
+          <option value="action">Action</option>
+          <option value="comedy">Comedy</option>
+          <option value="drama">Drama</option>
+          <option value="horror">Horror</option>
+          <option value="musical">Musical</option>
+          <option value="romance">Romance</option>
+          <option value="sci-fi">Sci-Fi</option>
+          <option value="western">Western</option>
+          <option value="documentary">Documentary</option>
+          <option value="animation">Animation</option>
+        </select>
+      </div>
+
       <div className="flex justify-between items-center">
-        <div className="flex-grow relative">
-          <select
-            value={sortBy}
-            onChange={(e) => setSortBy(e.target.value)}
-            className="flex border border-gray-400 p-2 text-black mb-4"
-          >
-            <option value="">Sort By</option>
-            <option value="az">A-Z</option>
-            <option value="za">Z-A</option>
-            <option value="low-high">Low-High</option>
-            <option value="high-low">High-Low</option>
-          </select>
-
-          <select
-            value={filterRating}
-            onChange={(e) => setFilterRating(e.target.value)}
-            className="flex border border-gray-400 p-2 text-black mb-4"
-          >
-            <option value="">Age Rating</option>
-            <option value="G">G</option>
-            <option value="PG">PG</option>
-            <option value="14A">14A</option>
-            <option value="18A">18A</option>
-            <option value="A">A</option>
-          </select>
-
-          <select
-            value={filterGenre}
-            onChange={(e) => setFilterGenre(e.target.value)}
-            className="flex border border-gray-400 p-2 text-black mb-4"
-          >
-            <option value="">Genre</option>
-            <option value="action">Action</option>
-            <option value="comedy">Comedy</option>
-            <option value="drama">Drama</option>
-            <option value="horror">Horror</option>
-            <option value="musical">Musical</option>
-            <option value="romance">Romance</option>
-            <option value="sci-fi">Sci-Fi</option>
-            <option value="western">Western</option>
-            <option value="documentary">Documentary</option>
-            <option value="animation">Animation</option>
-          </select>
-        </div>
+        <select
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+          className="flex border border-gray-400 p-2 text-black mb-4"
+        >
+          <option value="">Sort By</option>
+          <option value="az">A-Z</option>
+          <option value="za">Z-A</option>
+          <option value="low-high">Low-High</option>
+          <option value="high-low">High-Low</option>
+        </select>
       </div>
 
       <ul>
--- app/page.js
+++ app/page.js
@@ -5,8 +5,6 @@ import MovieList from "./movie-list";
 
 export default function Page() {
     const [movies, setMovies] = useState([]);
-    const [ageRating, setAgeRating] = useState("");
-    const [genre, setGenre] = useState(""); 
 
     const handleFetchedMovies = (fetchedMovies) => {
         setMovies(fetchedMovies);
@@ -15,43 +13,8 @@ export default function Page() {
     return (
         <main className="container mx-auto p-4">
             <h1 className="flex font-bold text-xl">Movie Suggestions 4 U</h1>
-
-            <div className="flex gap-4 mb-4" style={{visibility: 'hidden'}}>
-                <select
-                    value={ageRating}
-                    onChange={(e) => setAgeRating(e.target.value)}
-                    className="flex border border-gray-400 p-2 text-black"
-                >
-                    <option value="">Age Rating</option>
-                    <option value="G">G</option>
-                    <option value="PG">PG</option>
-                    <option value="14A">14A</option>
-                    <option value="18A">18A</option>
-                    <option value="A">A</option>
-                </select>
-
-                <select
-                    value={genre}
-                    onChange={(e) => setGenre(e.target.value)}
-                    className="flex border border-gray-400 p-2 text-black"
-                >
-                    <option value="">Genre</option>
-                    <option value="28">Action</option>
-                    <option value="35">Comedy</option>
-                    <option value="18">Drama</option>
-                    <option value="27">Horror</option>
-                    <option value="10402">Musical</option>
-                    <option value="10749">Romance</option>
-                    <option value="878">Sci-Fi</option>
-                    <option value="37">Western</option>
-                    <option value="99">Documentary</option>
-                    <option value="16">Animation</option>
-                </select>
-            </div>
-
+            
             <MovieFetcher 
-                ageRating={ageRating} 
-                genre={genre} 
                 onMoviesFetched={handleFetchedMovies} 
             />
             
