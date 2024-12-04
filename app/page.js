47df6478fc34412a1df9fa3c80c37f8ef841e162
Rgh2k4 <146884367+Rgh2k4@users.noreply.github.com>
2024-12-03 20:46:37 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Page() {
 
     return (
         <main className="flex-container mx-auto p-4">
-            <h1 classname="flex justify-center absolute top-0 font-bold gap-5 mb-4">Movie Suggestions 4 U</h1>
+            <h1 className="flex justify-center absolute top-0 font-bold gap-5 mb-4">Movie Suggestions 4 U</h1>
 
             <div>
                 <select
