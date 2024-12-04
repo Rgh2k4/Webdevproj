cf77198b95f5ae070dbe8011323ddaca81fe60c9
Rgh2k4 <146884367+Rgh2k4@users.noreply.github.com>
2024-12-03 17:38:52 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Page() {
         <main className="container mx-auto p-4">
             <h1 className="flex font-bold text-xl justify-center absolute top-0 left-1/2 transform -translate-x-1/2">Movie Suggestions 4 U</h1>
 
-            <div className="flex justify-center gap-5 mb-4 bottom-29">
+            <div className="flex justify-center gap-5 mb-4 top-30">
                 <select
                     value={ageRating}
                     onChange={(e) => setAgeRating(e.target.value)}
